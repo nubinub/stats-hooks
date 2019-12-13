f70972b3a52e288a0a524520f72c5b51509e3c75
nubinub <fab_thomas@msn.com>
2019-12-13 11:49:29 +0100

    test(max): enhance tests with floating numbers

--- src/utils/max.test.js
+++ src/utils/max.test.js
@@ -13,18 +13,23 @@ describe('utils/max', () => {
     expect(max([1])).toEqual(1);
   });
 
-  it('should return the greater element when multiple positive elements', () => {
+  it('should return the greater element when multiple positive integers', () => {
     expect(max([1, 3, 7, 4, 2])).toEqual(7);
     expect(max([1, 3, 7, 4, 2, 9, 10, 4])).toEqual(10);
   });
 
-  it('should return the greater element when multiple negative elements', () => {
+  it('should return the greater element when multiple negative integers', () => {
     expect(max([-1, -3, -7, -4, -2])).toEqual(-1);
     expect(max([-1, -3, -7, -4, -2, -9, -10, -4])).toEqual(-1);
   });
 
-  it('should return the greater element when both positive and negative elements', () => {
+  it('should return the greater element when both positive and negative intergers', () => {
     expect(max([1, -1, 2, -7])).toEqual(2);
     expect(max([-1, 3, 7, -4, 2, -9, -10, 4])).toEqual(7);
+  });
+
+  it('should return the greater element when both positive and negative floating numbers', () => {
+    expect(max([1.1, -1.5, 2.3, -7.0])).toEqual(2.3);
+    expect(max([-1.4, 3.2, 7.5, -4.4, 2, -9.0, -10.5, 4.3])).toEqual(7.5);
   });
 });
