ea2a9b951cae285ce83a36f327b1a8602f25fdc4
nubinub <fab_thomas@msn.com>
2019-12-20 17:29:29 +0100

    test(useFrequency): unit testing and quick fix

--- src/hooks/useFrequency/useFrequency.js
+++ src/hooks/useFrequency/useFrequency.js
@@ -4,27 +4,33 @@ import useMax from '../useMax';
 import useValues from '../useValues';
 
 const useFrequency = (data = [], step = 10, field = undefined) => {
-    const min = useMin(data, field);
-    const max = useMax(data, field);
-    const values = useValues(data, field);
+  const min = useMin(data, field);
+  const max = useMax(data, field);
+  const values = useValues(data, field);
 
-    const intervalMin = React.useMemo(() => {
-        return !isNaN(min) && step !== undefined ?  min - Math.abs(min) % step : NaN;
-    }, [min, step]);
+  const intervalMin = React.useMemo(() => {
+    if(isNaN(min) || step == undefined) {
+      return NaN;
+    }
+    if (min >= 0) {
+      return min - min % step;
+    }
+    return min - (step - (Math.abs(min) % step));
+  }, [min, step]);
 
-    return React.useMemo(() => {
-        const frequency = [];
-        if (!isNaN(intervalMin) && step !== undefined && !isNaN(max)) {
-            for(let i = intervalMin; i <= max; i += step) {
-                frequency.push({
-                    lowerBound: i,
-                    upperBound: i + step,
-                    n: values.filter((v) => v >= i && v < (i + step)).length
-                });
-            }
-        }
-        return frequency;
-    }, [intervalMin, max, values, step]);
+  return React.useMemo(() => {
+    const frequency = [];
+    if (!isNaN(intervalMin) && step !== undefined && !isNaN(max)) {
+      for (let i = intervalMin; i <= max; i += step) {
+        frequency.push({
+          lowerBound: i,
+          upperBound: i + step,
+          n: values.filter((v) => v >= i && v < (i + step)).length
+        });
+      }
+    }
+    return frequency;
+  }, [intervalMin, max, values, step]);
 };
 
 export default useFrequency;
